f523ecdc4db612cd3a446ad6a4beb73630089372
Chinmay Joshi <chinmay.joshi@uga.edu>
2023-04-12 06:05:39 -0400

    temp

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,6 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from .models import TodoItem
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    todo_list = TodoItem.objects.all()
+    output = ''
+    for t in todo_list:
+        output += t.title + ' ' + str(t.status) + ' \n'
+
+    return HttpResponse(output)
